4dd010d219fb3d79acb7e27dc00898efb5505d78
Dustin McQuay <dmcquay@gmail.com>
2010-11-28 11:43:10 -0700

    Implemented doOnCollection and added some tests.

--- test/DataProvider.js
+++ test/DataProvider.js
@@ -1,4 +1,5 @@
 var DataProvider = require('../lib/DataProvider').DataProvider,
+    Persistable = require('../lib/Persistable').Persistable,
     vows = require('vows'),
     assert = require('assert'),
     sys = require('sys');
@@ -8,6 +9,16 @@ var mongoConfig = {
     host: 'localhost',
     dbname: 'test'
 };
+
+//Implement a Persistable subclass
+var TestModel = function(raw) {
+    raw = raw || {};
+    Persistable.call(this);
+    this.raw = raw;
+};
+sys.inherits(TestModel, Persistable);
+TestModel.prototype.getCollectionName = function() { return 'test' };
+TestModel.prototype.toObject = function() { return this.raw };
 
 vows.describe('DataProvider').addBatch({
     'DataProvider': {
@@ -51,6 +62,28 @@ vows.describe('DataProvider').addBatch({
 
         'must be an instance of DataProvider': function(dp) {
             assert.ok(dp instanceof DataProvider);
-        } 
+        },
+
+        'should save': {
+            topic: function(dp) {
+                var model = new TestModel({id:1,foo:'bar'});
+                dp.save(model, this.callback);
+            },
+
+            'test': function(err, model) {
+                assert.ok(!err);
+            }
+        },
+
+        'should save using doOnCollection': {
+            topic: function(dp) {
+                var model = new TestModel({id:1,foo:'bar'});
+                dp.doOnCollection('test', 'insert', [{id:2,foo:'bar2'}], this.callback);
+            },
+
+            'test': function(err, docs) {
+                assert.ok(!err);
+            }
+        }
     }
 }).export(module);
